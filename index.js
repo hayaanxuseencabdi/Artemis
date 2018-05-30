52deac82d347a5165eab8627cf56b8e143310f94
Hayaan <hayaanabdi96+github@gmail.com>
2018-05-30 05:06:36 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var Discord = require("discord.js");
+var config = require("./config.json");
+var client = new Discord.Client();
+var prefix = config.prefix;
+client.on("ready", function () {
+    console.log("I am ready!");
+});
+client.on("message", function (message) {
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+    if (message.content.startsWith(prefix + "ping")) {
+        message.channel.send("pong!");
+    }
+    else if (message.content.startsWith(prefix + "foo")) {
+        message.channel.send("bar!");
+    }
+});
+client.login(config.artemis_token);
---
+++ index.ts
@@ -0,0 +1,21 @@
+declare function require(name:string);
+const Discord = require("discord.js");
+const config = require("./config.json");
+const client = new Discord.Client();
+const prefix = config.prefix;
+
+client.on("ready", () => {
+    console.log("I am ready!");
+});
+
+client.on("message", (message) => { 
+    if (!message.content.startsWith(prefix) || message.author.bot) 
+        { return; }
+    if (message.content.startsWith(prefix + "ping")) {
+        message.channel.send("pong!");
+    } else if (message.content.startsWith(prefix + "foo")) {
+        message.channel.send("bar!");
+    }
+});
+
+client.login(config.artemis_token);
