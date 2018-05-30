e5e84dbf22ba3824533181ea2749871b3647b144
Hayaan <hayaanabdi96+github@gmail.com>
2018-05-30 15:06:54 +0200

    Dissecting a message into its prefix and its commands

--- index.js
+++ index.js
@@ -2,21 +2,44 @@ var Discord = require("discord.js");
 var config = require("./config.json");
 var client = new Discord.Client();
 var prefix = config.prefix;
+
 client.on("ready", function () {
     console.log("I am ready!");
 });
+
 client.on("message", function (message) {
-    if (message.author.id === config.ownerID) {
-        message.channel.send("Hello Hayaan");
-    }
-    if (!message.content.startsWith(prefix) || message.author.bot) {
+    // Check author of the message, return if it isn't the creator a.k.a. me
+    // if (message.author.id === config.ownerID) {
+    //     message.channel.send("Hello Hayaan");
+    // } else 
+    if (message.content.startsWith(prefix) || !message.author.bot) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+        switch (command) {
+            case "ping":
+                message.channel.send("Pong!");
+                break;
+            case "blah":
+                message.channel.send("Meh.");
+                break;
+            default:
+                message.channel.send("Invalid command, try again.");
+                break;
+        }
+    } else {
         return;
     }
-    if (message.content.startsWith(prefix + "ping")) {
-        message.channel.send("pong!");
-    }
-    else if (message.content.startsWith(prefix + "foo")) {
-        message.channel.send("bar!");
-    }
+
+
+    // if (!message.content.startsWith(prefix) || message.author.bot) {
+    //     return;
+    // }
+    // if (message.content.startsWith(prefix + "ping")) {
+    //     message.channel.send("pong!");
+    // }
+    // else if (message.content.startsWith(prefix + "foo")) {
+    //     message.channel.send("bar!");
+    // }
 });
+
 client.login(config.artemis_token);
