732d98b0797aec27d5afd1995aa009103005346b
Hayaan <hayaanabdi96+github@gmail.com>
2018-06-02 22:11:40 +0200

    Succesfully able to access JSON's inner quote object's value to retrieve prices, percentual changes, etc of an input coin by its symbol

--- coins.js
+++ coins.js
@@ -1,9 +1,31 @@
 const coins = JSON.parse(JSON.stringify(require("./coins.json")));
-let coinsHashMap = new Map();
+const request = require("request");
 
-for (let i = 0; i < 1000; i++) {
-    coinsHashMap.set(coins.data[i].symbol, coins.data[i].name);
+let coinMap = new Map();
+
+for (let i = 0; i < 100; i++) {
+  let coinObject = {};
+  coinObject.id = coins.data[i].id;
+  coinObject.name = coins.data[i].name;
+  coinMap.set(coins.data[i].symbol, coinObject);
 }
-coinsHashMap.forEach((key, value) => { 
-  console.log(`Key: ${key} | Value: ${value}`);
-});
+// coinMap.forEach((key, value) => { 
+//   console.log(`Key: ${key.id} | Value: ${value}`);
+// });
+
+for (let i = 0; i < 1; i++) {
+  let coinURL = `https://api.coinmarketcap.com/v2/ticker/${coinMap.get("LTC").id}`;
+  request({
+    url: coinURL,
+    json: true },
+    (error, response, body) => { 
+      if (!error && response.statusCode === 200) {
+        let coinInfo = body.data;
+        console.log(JSON.parse(JSON.stringify(coinInfo.quotes.USD.percent_change_1h)));
+      } else {
+        console.log(`Error ${response.statusCode}`);
+      }
+    }
+  );
+}
+// console.log(coinMap.get("LTC"));
