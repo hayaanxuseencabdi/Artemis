82937e795309761fd63b43732e1f9ec8cce39a6c
Hayaan <hayaanabdi96+github@gmail.com>
2018-06-02 21:38:20 +0200

    hMap<symbol, name> is working, however I need to store the ID to be able to pull the data from CMC's API

--- coins.js
+++ coins.js
@@ -1,7 +1,9 @@
+const coins = JSON.parse(JSON.stringify(require("./coins.json")));
 let coinsHashMap = new Map();
-let coins = 
-Object.entries(coins).forEach(
-  ([symbol, name]) => { coinsHashMap.set(symbol, name);
-});
 
-console.log(coinsHashMap.get("BTC"));
+for (let i = 0; i < 1000; i++) {
+    coinsHashMap.set(coins.data[i].symbol, coins.data[i].name);
+}
+coinsHashMap.forEach((key, value) => { 
+  console.log(`Key: ${key} | Value: ${value}`);
+});
