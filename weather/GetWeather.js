02c60b5102bf6f1e38f6098da0f6b5ab5a4f88e1
Hayaan <hayaanabdi96+github@gmail.com>
2018-06-08 21:49:59 +0200

    WeatherEmbed title incompatible with mobile Discord

--- weather/GetWeather.js
+++ weather/GetWeather.js
@@ -7,7 +7,7 @@ const helper = require("../HelperFunctions");
 module.exports = {
   getWeather: async (message, args, footerPicture) => {
     let currentLocation = await location.returnCoordinates(message, args);
-    if (currentLocation === undefined) { return;; }
+    if (currentLocation === undefined) { return; }
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${currentLocation.latitude}&lon=${currentLocation.longitude}&appid=${process.env.OPENWEATHERMAP}`)
     .then((unparsedJSON) => unparsedJSON.json())
     .then((info) => {
@@ -15,12 +15,11 @@ module.exports = {
         info.main.temp, info.main.humidity, info.wind.speed,
         info.clouds.all, info.weather[0].icon);
       const weatherEmbed = new Discord.RichEmbed()
-        .setTitle(currentLocation.toString())
+        .setDescription(currentLocation.toString())
         .setColor((info.weather[0].icon[2] == 'd') ? 0xF4E542 : 0x000000)
         .setThumbnail(currentWeather.iconURL)
         .setURL(currentWeather.weatherURL)
-        .setDescription(`**Weather:** ${currentWeather.weatherConditions}\n` +
-          `**Temperature:** ${currentWeather.celsius} °C | ${currentWeather.fahrenheit} °F\n` + 
+        .addField(`${currentWeather.celsius} °C | ${currentWeather.fahrenheit} °F`, `**Weather:** ${currentWeather.weatherConditions}\n` + 
           `**Humidity:** ${currentWeather.humidity}%\n**Cloudiness:** ${currentWeather.cloudiness}%\n` +
           `**Wind speed:** ${currentWeather.windSpeedMetres} m/s | ${currentWeather.windSpeedMiles}mph`);
       message.channel.send(weatherEmbed);
