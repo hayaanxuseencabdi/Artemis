692176cf39c3d3a19a99ac520171ecf4ddcae00f
Hayaan <hayaanabdi96+github@gmail.com>
2018-06-17 14:05:22 +0200

    Encodes the Geocoding GET request, allowing users to use special characters

--- weather/LocationSearch.js
+++ weather/LocationSearch.js
@@ -5,7 +5,7 @@ const Location = require("./Location");
 
 module.exports = {
   returnCoordinates: (message, query) => {
-    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${HelperFunctions.transformToQuery(query)}&key=${process.env.GEOCODING}`)
+    return fetch(encodeURI(`https://maps.googleapis.com/maps/api/geocode/json?address=${HelperFunctions.transformToQuery(query)}&key=${process.env.GEOCODING}`))
       .then((info) => info.json()) 
       .then((infoJSON) => {
         if (infoJSON.status !== "OK") {
@@ -21,6 +21,8 @@ module.exports = {
         }
         const info = infoJSON.results[0].address_components;
         const address = infoJSON.results[0].formatted_address;
+        message.channel.send("Geocoding JSON");
+        message.channel.send(JSON.stringify(infoJSON, null, 2));
         const symbol = (info[info.length - 1].types[0] === "postal_code") ? info[info.length - 2].short_name.toLowerCase() : info[info.length - 1].short_name.toLowerCase();
         const coordinates = [infoJSON.results[0].geometry.location.lng, infoJSON.results[0].geometry.location.lat];
         return new Location(address, symbol, coordinates[0], coordinates[1]);
